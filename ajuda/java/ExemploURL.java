e7f0fd4ac9f29236bcc6182e5db37b16ee42a5ef
bigheadbh <felipebh@gmail.com>
2020-08-18 08:51:46 -0300

    new files!

---
+++ ajuda/java/ExemploURL.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.*;
+
+class ExemploURL {
+   public static String getHtml(String endereco){
+      URL url;
+      InputStream is = null;
+      BufferedReader br;
+      String resp = "", line;
+
+      try {
+         url = new URL(endereco);
+         is = url.openStream();  // throws an IOException
+         br = new BufferedReader(new InputStreamReader(is));
+
+         while ((line = br.readLine()) != null) {
+            resp += line + "\n";
+         }
+      } catch (MalformedURLException mue) {
+         mue.printStackTrace();
+      } catch (IOException ioe) {
+         ioe.printStackTrace();
+      } 
+
+      try {
+         is.close();
+      } catch (IOException ioe) {
+         // nothing to see here
+
+      }
+
+      return resp;
+   }
+   public static void main(String[] args) {
+      String endereco, html;
+      endereco = "http://maratona.crc.pucminas.br/series/Friends.html";
+      html = getHtml(endereco);
+      System.out.print(html);
+   }
+}
