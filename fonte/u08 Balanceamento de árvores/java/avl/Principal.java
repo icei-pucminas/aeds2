dea0b4ce771e5e80700630ea3aae01db386340dd
Felipe Domingos <felipebh@gmail.com>
2021-11-23 09:52:08 -0300

    format codes!

--- fonte/u08 Balanceamento de árvores/java/avl/Principal.java
+++ fonte/u08 Balanceamento de árvores/java/avl/Principal.java
@@ -6,11 +6,12 @@ public class Principal {
 	public static void main(String[] args) {
 		try {
 			AVL avl = new AVL();
-			int array[] = {1,2,3,4,5,6,7,8,9,10};
+			int array[] = {4,35,10,13,3,30,15,12,7,40,20};
 			for(int item: array){
+				System.out.println("Inserindo -> " + item);
 				avl.inserir(item);
+				avl.caminharPre();
 			}
-			avl.caminharPre();
 		} catch (Exception erro) {
 			System.out.println(erro.getMessage());
 		}
