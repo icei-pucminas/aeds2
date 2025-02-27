24ef4e5a050b3c0cada5dfffb572a9c43686fbfd
alexandria <alexandriapedroo@gmail.com>
2025-02-26 23:22:22 -0300

    question 4 done!!!

--- tps/entrada e saida/tp01/TP01Q04 - Alteração Aleatória/tp01q04.java
+++ tps/entrada e saida/tp01/TP01Q04 - Alteração Aleatória/tp01q04.java
@@ -1,20 +1,84 @@
+
+import java.util.Random;
+
 /*
- * Alteração Aleatória - Crie um m ́etodo iterativo que recebe uma string, sorteia duas letras
-min ́usculas aleat ́orias (c ́odigo ASCII ≥ ’a’ e ≤ ’z’), substitui todas as ocorrˆencias da primeira
-letra na string pela segunda e retorna a string com as altera ̧c ̃oes efetuadas. Na sa ́ıda padr ̃ao,
-para cada linha de entrada, execute o m ́etodo desenvolvido nesta quest ̃ao e mostre a string
-retornada como uma linha de sa ́ıda. Abaixo, observamos um exemplo de entrada supondo que
-para a primeira linha as letras sorteados foram o ’a’ e o ’q’. Para a segunda linha, foram o ’e’ e
-o ’k’.
-EXEMPLO DE ENTRADA: EXEMPLO DE SA ́IDA:
-o rato roeu a roupa do rei de roma o rqto roeu q roupq do rei de romq
-e qwe qwe qwe ewq ewq ewq k qwk qwk qwk kwq kwq kwq
-FIM
-A classe Random do Java gera n ́umeros (ou letras) aleat ́orios e o exemplo abaixo mostra uma
-letra min ́uscula na tela. Em especial, destacamos que: i) seed  ́e a semente para gera ̧c ̃ao de
-n ́umeros aleat ́orios; ii) nesta quest ̃ao, por causa da corre ̧c ̃ao autom ́atica, a seed ser ́a quatro; iii)
-a disciplina de Estat ́ıstica e Probabilidade faz uma discuss ̃ao sobre “aleat ́orio”.
-1 Random gerador = new Random ( ) ;
-2 gerador . setSeed ( 4 ) ;
-3 System . out . println ( ( char ) ( ’ a ’ + (Math. abs ( gerador . nextInt ( ) ) % 2 6 ) ) ) ;
+ * Alteração Aleatória - Crie um método iterativo que recebe
+ * uma string, sorteia duas letras minúsculas aleatórias 
+ * (código ASCII ≥ 'a' e ≤ 'z'), substitui todas as ocorrências
+ * da primeira letra na string pela segunda e retorna a string 
+ * com as alterações efetuadas. Na saída padrão, para cada linha
+ * de entrada, execute o método desenvolvido nesta questão e mostre
+ * a string retornada como uma linha de saída. Abaixo, observamos 
+ * um exemplo de entrada supondo que para a primeira linha as letras 
+ * sorteadas foram 'a' e 'q'. Para a segunda linha, foram 'e' e 'k'.
+ * __________________________________________________________________________
+ * Exemplo de Entrada:	               |Exemplo de Saída:
+ * o rato roeu a roupa do rei de roma  |o rqto roeu q roupq do rei de romq
+ * e qwe qwe qwe ewq ewq ewq	       |k qwk qwk qwk kwq kwq kwq
+ *           FIM	                   |                FIM
+ * ____________________________________|_____________________________________
+ * A classe Random do Java gera números (ou letras) aleatórios e o 
+ * exemplo abaixo mostra uma letra minúscula na tela. Em especial, 
+ * destacamos que: seed é a semente para geração de números aleatórios;
+ * nesta questão, por causa da correção automática, a seed será quatro;
+ * a disciplina de Estatística e Probabilidade faz uma discussão sobre
+ * "aleatório". code example bellow:
+ *  Random gerador = new Random();
+ *  gerador.setSeed(4);
+ *  System.out.println((char)('a' + (Math.abs(gerador.nextInt()) % 26)));
  */
+
+public class tp01q04 {
+    public static String randomAlterator(String sentence, Random generator) {
+        /*
+         * A String function that takes a sentence and
+         * randomizer it, using two random letters made 
+         * by the random method. If it sees the first 
+         * drawn letter in the sentece, it changes for
+         * the second one. 
+         */
+        
+        char firstDrawn = (char) ('a' + (Math.abs(generator.nextInt()) % 26));
+        //char firstDrawn = 'e';
+        char secondDrawn = (char) ('a' + (Math.abs(generator.nextInt()) % 26));
+        //char secondDrawn = 'f';
+
+        //MyIO.println(firstDrawn);
+        //MyIO.println(secondDrawn);
+        
+        String changedSentence = "";
+
+        for (int index = 0; index < sentence.length(); index++) {
+            if (sentence.charAt(index) == firstDrawn) {
+                changedSentence += secondDrawn;
+            } else {
+                changedSentence += sentence.charAt(index);
+            }
+        }
+
+        //MyIO.println("Sentença original: "+ sentence);
+        //MyIO.println("Sentença alterada: "+ changedSentence);
+
+        return changedSentence;
+    }
+
+    public static void main(String[] args) {
+        /*
+         * A Main function that reads a sentence
+         * and calls the random alterator method
+         * for it. In the final, it prints the
+         * changed sentence.
+         */
+
+        Random generator = new Random();
+        generator.setSeed(4);
+
+        String line = MyIO.readLine();
+
+        while (!"FIM".equals(line)) {
+            MyIO.println(randomAlterator(line, generator));
+
+            line = MyIO.readLine();
+        }
+    }
+}
