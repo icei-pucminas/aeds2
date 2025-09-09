5f63bcf13da31cbc7c1cfca7d4972a1a87d3d117
Matheus Barros Pereira <matheusbarrosp@gmail.com>
2025-09-09 20:17:32 -0300

    Update do código base da oficina 2

--- oficinas/Oficina2/codigo_base/exerc1.c
+++ oficinas/Oficina2/codigo_base/exerc1.c
@@ -40,26 +40,11 @@ void promoverTarefa(PilhaEmergencia* p, ListaBackground* l, int id){
 void empilharEmergencia(PilhaEmergencia* p, Tarefa t){
     // TO DO: IMPLEMENTAR
     // empilhar uma tarefa na pilha
-    if(p->n >= MAX_EMERGENCIA){ // pilha ta cheia
-	printf("Pilha cheia\n");
-    }else{
-    	p->pilha[p->n] = t;
-	p->n = p->n+1;
-    }
 }
 
 Tarefa desempilharEmergencia(PilhaEmergencia* p){
     // TO DO: IMPLEMENTAR
     // desempilhar uma tarefa da pilha
-    if(p->n == 0){
-	    printf("Pilha vazia");
-	    return null;
-    }else{
-	Tarefa desempilhada;
-	desempilhada = p->pilha[p->n];
-	p->n = p->n - 1;
-	return desempilhada;
-    }
 }
 
 void enfileirarPeriodica(FilaPeriodica* f, Tarefa t){
@@ -137,3 +122,4 @@ int main(){
     }
     return 0;
 }
+
